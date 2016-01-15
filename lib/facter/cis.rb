6440cdd50015a125b82dd389ea1edabbf90a30a6
Benjamin Fischer <benjamin.fischer@schufa.de>
2016-01-15 20:30:17 +0100

    fix

--- lib/facter/cis.rb
+++ lib/facter/cis.rb
@@ -1,15 +1,16 @@
 require 'facter'
 
 # Check to see if rz.conf is there
-Facter.add(:cis_is_tmpseperatemount) do
+Facter.add(:cis) do
   confine :osfamily => "RedHat"
   cishash = {}
   setcode do
     returnval = Facter::Core::Execution.exec('grep "[[:space:]]/blah[[:space:]]" /etc/fstab')
     if returnval != ''
-      true
+      cishash['is_tmpseperatemount'] =  true
     else
-      false
+      cishash['is_tmpseperatemount'] =  false
     end
+    cishash
   end
 end
--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -12,7 +12,7 @@
 #
 class cisbench::filesystem (
   $tmpseperatemount_report = $cisbench::params::tmpseperatemount_report,) inherits cisbench::params {
-  if $::cis_is_tmpseperatemount == false {
+  if $::cis['is_tmpseperatemount'] == false {
     notify { "/tmp is not on a separate mount. Failed tmpseperatemount_report check.": }
   }
 }
