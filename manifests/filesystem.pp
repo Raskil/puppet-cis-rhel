20781db00e725b9354e700b6f2829bbe3979a9dd
Benjamin Fischer <benjamin.fischer@schufa.de>
2016-01-15 19:59:02 +0100

    fix

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -12,7 +12,8 @@
 #
 class cisbench::filesystem (
   $tmpseperatemount_report = $cisbench::params::tmpseperatemount_report,) inherits cisbench::params {
-    notify {"${::cis}"}
+  notify { "${::cis}": }
+
   if $::is_tmpseperatemount == false {
     notify { "/tmp is not on a separate mount. Failed tmpseperatemount_report check.": }
   }
