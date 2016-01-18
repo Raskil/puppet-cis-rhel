82a50c4389d5f5c014c36608115465a01a87c42c
Benjamin Fischer <benjamin.fischer@schufa.de>
2016-01-18 18:06:35 +0100

    fix for output of modprobe

--- lib/facter/cis.rb
+++ lib/facter/cis.rb
@@ -87,43 +87,43 @@ Facter.add(:cis) do
       returnval = Facter::Core::Execution.exec('df --local -P | awk {\'if (NR!=1) print $6\'} | xargs -I \'{}\' find \'{}\' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null')
       cishash['world_writeable_dirs_without_sb'] = returnval.split(/\n+/)
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v cramfs')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v cramfs 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_cramfsdisabled'] =  true
     else
       cishash['is_cramfsdisabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v freevxfs')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v freevxfs 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_freevxfsdisabled'] =  true
     else
       cishash['is_freevxfsdisabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v jffs2')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v jffs2 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_jffs2disabled'] =  true
     else
       cishash['is_jffs2disabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v hfs')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v hfs 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_hfsdisabled'] =  true
     else
       cishash['is_hfsdisabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v hfsplus')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v hfsplus 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_hfsplusdisabled'] =  true
     else
       cishash['is_hfsplusdisabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v squashfs')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v squashfs 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_squashfsdisabled'] =  true
     else
       cishash['is_squashfsdisabled'] =  false
     end
-    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v udf')
+    returnval = Facter::Core::Execution.exec('/sbin/modprobe -n -v udf 2>&1')
     if returnval.include? 'install /bin/true'
       cishash['is_udfdisabled'] =  true
     else
