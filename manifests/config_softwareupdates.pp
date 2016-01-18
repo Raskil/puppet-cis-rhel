6384ac8b098256053e504f11dad4fe112983543b
Benjamin Fischer <benjamin.fischer@schufa.de>
2016-01-18 20:53:07 +0100

    fix

--- manifests/config_softwareupdates.pp
+++ manifests/config_softwareupdates.pp
@@ -14,7 +14,7 @@ class cisbench::config_softwareupdates (
   $securityupdatesinstallled_report = $cisbench::params::securityupdatesinstallled_report,) inherits cisbench::params {
   ensure_packages('yum-plugin-security')
 
-  if $securityupdatesinstallled_report == true and $::cis['has_securityupdatesinstallled'] == false {
+  if $securityupdatesinstallled_report == true and $::cis['has_securityupdatesinstallled'] == true {
     notify { "Cisbench: System has available security patches. System needs to be updated!": after => Package['yum-plugin-security'
         ], }
   }
