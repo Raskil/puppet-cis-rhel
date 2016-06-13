a23aefe8ff546f7e855bbfe461f9934494e94e80
Benjamin Fischer <benjamin.fischer@schufa.de>
2016-06-13 16:16:51 +0200

    fir for fact is_selinuxbootenabled using wrong logic

--- lib/facter/cis.rb
+++ lib/facter/cis.rb
@@ -186,7 +186,7 @@ Facter.add(:cis) do
       end
     end
     returnval = Facter::Core::Execution.exec('grep selinux=0 /etc/grub.conf')
-    if returnval == '0'
+    if returnval == '1'
       cishash['is_selinuxbootenabled'] =  true
     else
       cishash['is_selinuxbootenabled'] =  false
